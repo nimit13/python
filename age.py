e7b3e3dfae6c2b372b64613a31310497a27cfc46
Your Name <ec2-user@ip-172-31-11-48.ap-south-1.compute.internal>
2019-06-12 11:24:23 +0000

    initial commit

---
+++ age.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+import datetime
+
+
+x=datetime.datetime.now()
+x=x.year
+
+
+name=input("Enter your name ")
+age=int(input("enter your age "))
+year=x-age+95
+print("you will turn 95 in ",year)
+
